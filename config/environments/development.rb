11ff529b2a7ef564120a90ba604b1eccca2874d2
Buttice Jeremy <buttice.j@gmail.com>
2014-08-18 17:50:40 +0200

    Add locales fr, devise, rails_admin, global

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,6 +6,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  #i18n
+  config.i18n.available_locales = :fr
+
   # Do not eager load code on boot.
   config.eager_load = false
 
